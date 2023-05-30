9b1d918bd5628834caf3e4d84196a331770d2bb0
syamfarh <e0939383@u.nus.edu>
2023-05-30 13:04:58 +0800

    completed Welcome page layout, completed Login page layout, backend still not fully complete

--- mindMap/routes/Welcome.js
+++ mindMap/routes/Welcome.js
@@ -1,4 +1,4 @@
-import { Image, View, TextInput, Button, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { Image, View, TextInput, Button, StyleSheet, TouchableOpacity, Text, ImageBackground } from 'react-native';
 
 export default function App({ navigation }) {
     const pressedLog = () => {
@@ -9,42 +9,52 @@ export default function App({ navigation }) {
         navigation.navigate("Registration");
       }
     return (
-        <View style = {styles.viewStyle}>
-            <View style={styles.picComponent}>
-                <Image source={require('../assets/adaptive-icon.png')} style={styles.imageStyle} />
-                <Text style={styles.picText}>
-                    MindMap
-                </Text>
-            </View>
-            <TouchableOpacity style={styles.login} onPress={pressedLog}>
-                <Text style={styles.loginText}>Login</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.register} onPress={pressedReg}>
-                <Text style={styles.registerText}>Register</Text>
-            </TouchableOpacity>
+        <View >
+            <ImageBackground source={require('../assets/skyblue.jpg')} style={styles.background} >
+                <View style={styles.picComponent}>
+                    <Image source={require('../assets/moonchild.jpg')} style={styles.imageStyle} />
+                    <Text style={styles.picText}>
+                        MindMap
+                    </Text>
+                </View>
+                <TouchableOpacity style={styles.login} onPress={pressedLog}>
+                    <Text style={styles.loginText}>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.register} onPress={pressedReg}>
+                    <Text style={styles.registerText}>Register</Text>
+                </TouchableOpacity>
+            </ImageBackground>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    background: {
+        width:500,
+        height:1000,
+        opacity:0.9,
+    },
     imageStyle: {
-        height: 58,
-        width: 58,
+        height: 150,
+        width: 150,
+        borderRadius: 100,
     },
 
     picComponent: {
         alignItems: 'center',
         justifyContent: 'center',
-        height: 99,
-        width: 141,
-        top: 490,
+        left:100,
+        height: 100,
+        width: 200,
+        top: 300,
         borderColor: '#1E232C',
         borderWidth: 0,
     },
 
     picText: {
         textAlignVertical: 'top',
-        fontSize: 20,
+        fontSize: 40,
+        fontWeight: "bold",
     },
 
     viewStyle: {
@@ -59,6 +69,7 @@ const styles = StyleSheet.create({
         width: 331,
         height: 56,
         top: 510,
+        left:37,
         borderRadius: 8,
     },
     loginText: {
@@ -80,6 +91,7 @@ const styles = StyleSheet.create({
         height: 56,
         top: 520,
         borderRadius: 8,
+        left:37,
         borderColor: '#1E232C',
     },
 
