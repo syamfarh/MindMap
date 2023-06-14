ea10f8a48462d770f24b20e437361892c3c78617
syamfarh <e0939383@u.nus.edu>
2023-06-14 13:52:42 +0800

    Added calendar to mood screen

--- mindMap/routes/Mood.js
+++ mindMap/routes/Mood.js
@@ -1,23 +1,31 @@
-import {Alert, View, TextInput, Button, StyleSheet, TouchableOpacity, Text, SafeAreaView, Modal, Image} from 'react-native';
-
+import {Alert, View, TextInput, Button, StyleSheet, TouchableOpacity, Text, Image} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Calendar } from 'react-native-calendars';
 
 export default function App({ navigation }) {
-    return (
+    const [isVisible, setIsVisible] = useState(false);
+    const [selected, setSelected] = useState('');
+    
+    return ( isVisible ? (
         <View style={styles.trueView}>
-            <Text style={styles.headerText}>How are you feeling today?</Text>
-            <View style={styles.emojiView}>
-                <TouchableOpacity>
-                    <Image source={require('../assets/angry.png')} style={styles.imageStyle}/>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Image source={require('../assets/laughing-emoji.png')} style={styles.imageStyle}/>    
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Image source={require('../assets/sad.png')} style={styles.imageStyle}/>
-                </TouchableOpacity>
-            </View>
+        <Text style={styles.headerText}>How are you feeling today?</Text>
+        <View style={styles.emojiView}>
+            <TouchableOpacity>
+                <Image source={require('../assets/angry.png')} style={styles.imageStyle}/>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Image source={require('../assets/laughing-emoji.png')} style={styles.imageStyle}/>    
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Image source={require('../assets/sad.png')} style={styles.imageStyle}/>
+            </TouchableOpacity>
         </View>
-        
+        </View>) :
+        <View>
+            <Calendar style= {styles.calendarStyle} onDayPress={day => { setSelected(day.dateString);}}
+                      markedDates={{[selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}}}
+            />
+        </View>
     );
   }
 
@@ -46,4 +54,9 @@ const styles = StyleSheet.create({
         width: 100,
         borderRadius: 10,
     },
+
+    calendarStyle: {
+        top: 100,
+        height: 350,
+    }
 });
