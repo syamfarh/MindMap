e9553a8867d052ff40155aedf4c66056bb7d1eef
Alteqa <e0969816@u.nus.edu>
2023-07-19 18:52:06 +0800

    added add to calendar button

--- mindMap/routes/Event.js
+++ mindMap/routes/Event.js
@@ -13,6 +13,30 @@ import { createJournal, editJournal } from "../helper";
 import { auth } from "../firebase-setup";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { waitForPendingWrites } from "firebase/firestore";
+// import CalendarEvents from "react-native-calendar-events";
+import RNCalendarEvents from "react-native-calendar-events";
+
+const checkCalendarPermissions = async () => {
+  try {
+    const status = await RNCalendarEvents.checkPermissions({
+      readOnly: false, // Set this to true if you only need read access to calendar events
+    });
+
+    if (status === "authorized") {
+      // User has granted calendar permissions
+      console.log("Calendar permissions granted.");
+    } else if (status === "denied") {
+      // User has denied calendar permissions
+      console.log("Calendar permissions denied.");
+    } else {
+      // Permissions have not been requested yet
+      console.log("Calendar permissions not requested yet.");
+    }
+  } catch (error) {
+    // Handle any error that occurred while checking permissions
+    console.log("Error checking calendar permissions:", error);
+  }
+};
 
 export default function App({ route, navigation }) {
   const { item } = route.params;
@@ -21,10 +45,13 @@ export default function App({ route, navigation }) {
     <View style={styles.container}>
       <View style={styles.box}>
         <Text style={styles.headerText}>{item.title}</Text>
-        <Text style={styles.date}>Date: {item.date}</Text>
+        <Text style={styles.date}>{item.date}</Text>
         <View style={styles.subBox}>
           <Text style={styles.description}>{item.description}</Text>
         </View>
+        <TouchableOpacity>
+          <Text style={styles.button}>Add to calendar +</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -48,6 +75,7 @@ const styles = StyleSheet.create({
     borderRightColor: "#aaa",
     borderBottomWidth: 10,
     borderRightWidth: 10,
+    // alignContent: "center",
   },
   headerText: {
     fontSize: 30,
@@ -57,6 +85,8 @@ const styles = StyleSheet.create({
   },
   date: {
     margin: 20,
+    fontWeight: "bold",
+    fontSize: 15,
   },
   subBox: {
     padding: 20,
@@ -69,5 +99,19 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 20,
     // fontWeight: "bold",
+  },
+  button: {
+    fontSize: 20,
+    color: "#555",
+    backgroundColor: "#ddd",
+    padding: 5,
+    textAlign: "center",
+    textAlignVertical: "center",
+    height: 50,
+    borderRadius: 30,
+    marginTop: 10,
+    borderWidth: 3,
+    borderColor: "#aaa",
+    fontWeight: "bold",
   },
 });
